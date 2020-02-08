12b15e81cd645ee12dc8ee76cf9f76939a58ef41
Shinichi Suwa <suwash01@gmail.com>
2020-02-08 13:07:32 +0900

    feat: prod/vpc enable DNS

--- terraform/environments/stg/main.tf
+++ terraform/environments/stg/main.tf
@@ -31,8 +31,8 @@ resource "aws_vpc" "vpc" {
   cidr_block                       = "10.100.0.0/16"
   enable_classiclink               = "false"
   enable_classiclink_dns_support   = "false"
-  enable_dns_hostnames             = "false"
-  enable_dns_support               = "false"
+  enable_dns_hostnames             = "true"
+  enable_dns_support               = "true"
   instance_tenancy                 = "default"
 
   tags = {
